7156beb743279a3a966e092c65d31cb6c49e6e56
Eduardo Perez <edujpp1@gmail.com>
2019-07-13 20:30:25 -0300

    Se modifico el componente Noticia utilizando cards de Materialize para mostrar la imagen, el titulo, la descripcion de la noticia y redireccionar a la misma

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-const Noticia = () => {
+/*
+  Un buen lugar para escribir código estándar de JS es antes del return
+  por eso en este caso se está dejando aunque sea un stateless functional
+  component. El no colocarlo se hace como una manera de resumir código.
+*/
+const Noticia = ({noticia}) => {
+
+  // Extraer los datos
+
+  const { urlToImage, url, title, description, source } = noticia;
+
   return(
-    <h1>Desde Noticia</h1>
+    <div className="col s12 m6 l4">
+      <div className="card">
+
+        <div className="card-image">
+          <img src={urlToImage} alt={title}/>
+        </div>
+        <div className="card-content">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="card-action">
+          <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-light btn">
+            Ver noticia completa
+          </a>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
