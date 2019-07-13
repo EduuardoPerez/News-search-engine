711742208350c0539d657a2cf3e1db1a80401eb7
Eduardo Perez <edujpp1@gmail.com>
2019-07-13 18:37:04 -0300

    Se realizó la consulta general de la API news API desde el componentDidMount a traves de la arrow function consultarNoticias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import { async } from 'q';
 
 class App extends Component{
   state = {}
+
+  // Se recomienda llamar una API cuando el documento está listo
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ar&apiKey=64894da34df448a5af3268a7a15778ea`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias: noticias.articles
+    })
+  }
+
   render(){
     return(<h1>Noticias API React</h1>);
   }
